8f29ddd70d390b5c3fecf104cd240301111448ae
SvetlanaCherusheva <58355788+SvetlanaCherusheva@users.noreply.github.com>
2021-12-06 19:35:43 +0300

    Add files via upload

---
+++ Тестирование/Задание 1/tests/test_division_fuc.py
@@ -0,0 +1,20 @@
+from solution import division
+import pytest
+
+
+@pytest.mark.parametrize("a, b, expected_result", [
+    (10, 2, 5),
+    (20, 10, 2),
+    (30, -3, -10),
+    (5, 2, 2.5)])
+def test_division_good(a, b, expected_result):
+    assert division(a, b) == expected_result
+
+@pytest.mark.parametrize("exepected_exception, devisionable, divider",[
+    (ZeroDivisionError, 10, 0),
+    (TypeError, 20, "2")
+])
+def test_division_with_error(exepected_exception, devisionable, divider):
+    with pytest.raises(exepected_exception):
+        division(devisionable, divider)
+
---
+++ Тестирование/Задание 2/tests/test_file_worker/conftest.py
@@ -0,0 +1,6 @@
+import pytest
+@pytest.fixture(autouse=True)
+
+def clean_text_file():
+    with open("../testfile.txt", "w"):
+        pass  # затирает файл
